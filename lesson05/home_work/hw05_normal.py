70289b8ca9f10a0ac21c6dca84267de9b3de42aa
vu9x <nguyentuanvu9x@gmail.com>
2019-01-28 15:39:51 +0300

    Сделано hw05 normal

--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,53 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+
+
+def go_to_dir():
+    dir_name = input("Ввдение название папки куда зайти: ")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    os.chdir(dir_path)
+    print(os.getcwd())
+
+
+def current_files():
+    print(os.listdir())
+
+
+def delete_dir(dir_name):
+    dir_name = input("Ввдение название новой папки: ")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.rmdir(dir_path)
+        print("директория {} удалена".format(dir_path))
+    except NotImplementedError:
+        print("директория {} не существует".format(dir_path))
+
+
+def make_dir(dir_name):
+    dir_name = input("Ввдение название новой папки: ")
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print("создана директория {}".format(dir_path))
+    except FileExistsError:
+        print("директория {} уже существует".format(dir_path))
+
+print(
+    "Выберите команду: \n1. Перейти в папку  \n2. Просмотреть содержимое текущей папки \n3. Удалить папку \n4. Создать папку"
+)
+
+do = input("Укажите действие: ")
+
+if do == "1":
+    go_to_dir()
+elif do == "2":
+    current_files()
+elif do == "3":
+    delete_dir()
+elif d0 == "4":
+    make_dir()
+
